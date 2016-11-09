ca66bbc446302ae75f7d619464addecda6b5438c
MiguePL <miguepl95@gmail.com>
2016-11-09 16:48:27 +0100

    SubidaInicial
    Archivos con el proyecto inicial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float velocidad = 5f;
+	public float power = 1f;
+	public float salto = 400;
+	public bool tocando_suelo = false;
+	private Animator animator;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat ("velocidad", Mathf.Abs (rb.velocity.x)); 
+		
+		if (Input.GetKey ("right")) {
+			animator.SetFloat ("velocidad", 1f);
+			rb.velocity = new Vector2 (velocidad*power,rb.velocity.y);
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if (Input.GetKey("left")) {
+			animator.SetFloat ("velocidad", 1f);
+			rb.velocity = new Vector2 (-velocidad*power, rb.velocity.y);
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			animator.SetBool  ("jump", true);
+			rb.AddForce (transform.up*salto);
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			animator.SetBool ("jump", false);
+		}
+			
+	}
+
+	void OnTriggerExit2D(Collider2D objeto){
+		if (objeto.tag == "Suelo") {
+			tocando_suelo = false;
+		}
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D objeto){
+		if (objeto.tag == "Suelo") {
+			tocando_suelo = true;
+			animator.SetBool ("jump", false);
+		}
+	
+	}
+
+}
