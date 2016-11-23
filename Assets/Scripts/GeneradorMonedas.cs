eabbad6e431251b4b387fbf633759e9f04db762b
MiguePL <miguepl95@gmail.com>
2016-11-23 18:40:35 +0100

    Caja
    Animacion de caja

--- Assets/Scripts/GeneradorMonedas.cs
+++ Assets/Scripts/GeneradorMonedas.cs
@@ -2,13 +2,27 @@
 using System.Collections;
 
 public class GeneradorMonedas : MonoBehaviour {
-	public GameObject moneda_nueva;
-	public GameObject moneda;
+	private GameObject moneda_nueva;
+	public Transform pos_salida;
+	public GameObject [] monedas;
+	private int numero_moneda = 0;
+	Animator anim;
 
-	void OnTriggerEnter2D(Collider2D objeto){
-		if(objeto.tag == "Player" &&  moneda_nueva == null) {
-			moneda_nueva = (GameObject)Instantiate (moneda, transform.position, transform.rotation);
+	void Start(){
+		pos_salida = transform.Find ("posicion_salida").transform;
+		Debug.Log ("Cantidad de monedas: " + monedas.Length);
+		anim = GetComponent<Animator> ();
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player" ) {
+			numero_moneda = Random.Range (0, monedas.Length - 1);
+			moneda_nueva = (GameObject)Instantiate (monedas[numero_moneda], pos_salida.position, transform.rotation);
+			anim.SetTrigger ("bote");
 		}
 	}
-
+	void OnTriggerEnter2D(Collider2D objeto){
+		if(objeto.tag == "Player" && moneda_nueva == null) {
+			moneda_nueva = (GameObject)Instantiate (monedas[numero_moneda], transform.position, transform.rotation);
+		}
+	}
 }
